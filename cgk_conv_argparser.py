66f2852d384ab4a6e9110269970c980508b83f68
serinko <johanesxsalmon@riseup.net>
2022-10-25 09:19:11 +0200

    testing parser implementation

--- cgk_conv_argparser.py
+++ cgk_conv_argparser.py
@@ -1,6 +1,6 @@
 """Simple commandline tool to get actual price of any coin on Coingecko and its multiples, modified with argparse"""
 
-import sys
+#import sys
 import requests
 from tabulate import tabulate
 import numpy as np
@@ -10,10 +10,11 @@ import argparse
 
 
 def get_pycoingecko_ids():
+    """Gets a list of all coin ids from coingecko API."""
     url = 'https://api.coingecko.com/api/v3/coins/list'
 
-    headers = {"id": "accept: application/json"}
-    r = requests.get(url, headers)
+#    headers = {"id": "accept: application/json"}
+    r = requests.get(url)
     # print(f"Status code: {r.status_code}")
     response_dicts = r.json()
     return response_dicts
@@ -54,26 +55,26 @@ def display_result(id,vs,amount,x_price,switch):
     print(msg)
 
 
-def display_help():
-    """Frontend display menu."""
-    msg_0 = "\nCOINGECKO CMD CONVERTOR"
-    line = "========================"
-    msg_1 = "Usage: \npython3 cgk_conv.py <id> <vs_currency> <amount>"\
-            "\n* Example: How much USD is 100 Monero:"\
-            "\n$ python3 cgk_conv.py monero usd 100"
-    msg_2 = "Optional argument <switch>:"\
-            "\n* Switch nominator and denominator."\
-            "\n$ python3 cgk_conv.py <id> <vs_currency> <amount> <switch>"\
-            "\n* Example: How much Monero is 100 USD?, type:"\
-            "\n$ python3 cgk_conv.py monero usd 100 switch"
-    msg_3 = "------------------------\nOptional Commands:"
-    tab = [
-        ["$ python3 cgk_conv.py id_list","Prints all IDs (more than 13000 items!!)"],
-        ["$ python3 cgk_conv.py id_less","Prints a short list of IDs"],
-        ["$ python3 cgk_conv.py vs_list", "Prints a list of vs_currencies (60 items)"],
-        ]
-    print(f"{msg_0}\n{line}\n{msg_1}\n{msg_2}\n{msg_3}")
-    print(tabulate(tab))
+#def display_help():
+#    """Frontend display menu."""
+#    msg_0 = "\nCOINGECKO CMD CONVERTOR"
+#    line = "========================"
+#    msg_1 = "Usage: \npython3 cgk_conv.py <id> <vs_currency> <amount>"\
+#            "\n* Example: How much USD is 100 Monero:"\
+#            "\n$ python3 cgk_conv.py monero usd 100"
+#    msg_2 = "Optional argument <switch>:"\
+#            "\n* Switch nominator and denominator."\
+#            "\n$ python3 cgk_conv.py <id> <vs_currency> <amount> <switch>"\
+#            "\n* Example: How much Monero is 100 USD?, type:"\
+#            "\n$ python3 cgk_conv.py monero usd 100 switch"
+#    msg_3 = "------------------------\nOptional Commands:"
+#    tab = [
+#        ["$ python3 cgk_conv.py id_list","Prints all IDs (more than 13000 items!!)"],
+#        ["$ python3 cgk_conv.py id_less","Prints a short list of IDs"],
+#        ["$ python3 cgk_conv.py vs_list", "Prints a list of vs_currencies (60 items)"],
+#        ]
+#    print(f"{msg_0}\n{line}\n{msg_1}\n{msg_2}\n{msg_3}")
+#    print(tabulate(tab))
 
 def display_id_list():
     """Downloads long list of IDs and prints them as a table."""
@@ -145,97 +146,78 @@ def get_sorted_array(lst,columns=3):
     return arr
     
     
-def main_app():
-    """Runs the main program based on passed options"""
-    #print(sys.argv)
-    if len(sys.argv) < 3:
-        if sys.argv[-1] == "id_list":
-            display_id_list()
-        elif sys.argv[-1] == "id_less":
-            display_id_less()
-        elif sys.argv[-1] == "vs_list":
-            display_vs_currencies()
-        else:
-            display_help()
-    else:
-        id = sys.argv[1].lower()
-        vs = sys.argv [2].lower()
-        if len(sys.argv) == 3 or len(sys.argv) == 4 and sys.argv[-1] == "switch":
-            amount = 1
-        else:
-            amount = float(sys.argv[3])
-        if sys.argv[-1].lower() == 'switch':
-            switch = True 
-        else:
-            switch = False
-        x_price = calc_x_price(id,vs,amount,switch)
+#def main_app():
+#    """Runs the main program based on passed options"""
+#    #print(sys.argv)
+#    if len(sys.argv) < 3:
+#        if sys.argv[-1] == "id_list":
+#            display_id_list()
+#        elif sys.argv[-1] == "id_less":
+#            display_id_less()
+#        elif sys.argv[-1] == "vs_list":
+#            display_vs_currencies()
+#        else:
+#            display_help()
+#    else:
+#        id = sys.argv[1].lower()
+#        vs = sys.argv [2].lower()
+#        if len(sys.argv) == 3 or len(sys.argv) == 4 and sys.argv[-1] == "switch":
+#            amount = 1
+#        else:
+#            amount = float(sys.argv[3])
+#        if sys.argv[-1].lower() == 'switch':
+#            switch = True 
+#        else:
+#            switch = False
+#        x_price = calc_x_price(id,vs,amount,switch)
 
-        display_result(id,vs,amount,x_price,switch)
+#        display_result(id,vs,amount,x_price,switch)
 
 def parser_main():
-    
-    parser = argparse.ArgumentParser()          
+    """Main function initializing ArgumentParser, storing arguments and executing commands."""    
+    parser = argparse.ArgumentParser(
+            prog='Coingecko Commandline Convertor',
+            description='''Convert any asset in any amount in terminal.''',
+            epilog='''Let there be dark!'''
+        )
     
     parser.add_argument("id", nargs='?', default="store_false",
                     help="add an id of an asset to convert")
     parser.add_argument("vs_currency", nargs='?', 
                     help="add a symbol of a currency to convert to")
     parser.add_argument("amount", type=float, nargs='?',default=1,
-                    help="price multiplier")
+                    help="price multiplier, default = 1")
+
     parser.add_argument("-s", "--switch",
                     action="store_true",
-                    help="switches id and vs_currency")
-    
-        
+                    help="switches: id <--> vs_currency (xmr to usd <--> usd to xmr)")       
     parser.add_argument("--id_list",
                     help="displays all convertable coin ids (over 13000 items!)", action="store_true")
     parser.add_argument("--id_less",
                     help="displays shortened list of convertable coin ids", action="store_true")
     parser.add_argument("--vs_list",
                     help="displays all vs currencies (~60items)", action="store_true")  
-
+    parser.add_argument("-V","--version", action="version", version='%(prog)s 2.0.1')
+    
+    
     args = parser.parse_args()
 
-    if args.id and args.vs_currency:
-        print(f"id = {args.id}")
-        print(f"vs = {args.vs_currency}")
-        print(f"amount = {args.amount}")
-        print(f"sw = {args.switch}")
-    else:
-        print(f"amount = None")
-        if args.id_list:
-            print(f"id_list = {args.id_list}")
-        elif args.vs_list:
-            print(f"vs_list = {args.vs_list}")
-        elif args.id_less:
-            print(f"id_less = {args.id_less}")
-    
-    
-    
-#    a = args.id
-#    b = args.vs_currency
-#    n = args.amount
-#    
-#    if args.switch == True:
-#        s = True
-#    else:
-#        s = False
-#    
-#    if args.id == False or args.vs_currency == False:
-#        n = False
-#        if args.id_list:
-#            display_id_list()
-#        elif args.id_less:
-#            display_id_less()
-#        elif args.vs_list:
-#            display_vs_currencies()
+    a = args.id
+    b = args.vs_currency
+    n = args.amount
+    s = args.switch
 
-#    if args.amount:
-#        x_price = calc_x_price(a,b,n,s)
-#        display_result(a,b,n,x_price,s)
-#    else:
-#        x_price = calc_x_price(a,b,n,s)
-#        display_result(a,b,n,x_price,s)
+    if a and b:
+        x_price = calc_x_price(a,b,n,s)
+        display_result(a,b,n,x_price,s)
+    else:
+        if args.id_list:
+            display_id_list()
+        elif args.id_less:
+            display_id_less()
+        elif args.vs_list:
+            display_vs_currencies()
+    
 
 if __name__ == '__main__':
 #    main_app()
