3ddb0b02719bb7b280cc536662177e67dc0a5294
serinko <johanesxsalmon@riseup.net>
2022-10-27 14:08:54 +0200

    commandline API crypto convertor

--- cgk.py
+++ cgk.py
@@ -101,6 +101,7 @@ def display_list(args):
         display_vs_currencies()
         
 def panic(msg):
+    """Error message print"""
     print(f"error: {msg}", file=sys.stderr)
     sys.exit(-1)
     
@@ -144,11 +145,11 @@ def parser_main():
             epilog=Style.DIM + '''Let there be dark!''' + Style.RESET_ALL
         )
     parser.add_argument("-V","--version", action="version", version='%(prog)s 2.5')
-    # List sub-command parser
+    # Sub-command parsers
     subparsers = parser.add_subparsers(help="{subcommand}[-h] shows all the options")
     parser_convert = subparsers.add_parser('convert',help='{C}[id][vs_currency]([--amount][--switch]) - example: ./cgk.py C monero btc 10', aliases=['C'])
     parser_list = subparsers.add_parser('list', help='displays list of convertable coins {L}[--argument]',aliases=['L'])
-            
+    # Convert - arguments        
     parser_convert.add_argument("id",
                     help="add an id of an asset to convert")
     parser_convert.add_argument("vs_currency",
@@ -158,54 +159,23 @@ def parser_main():
     parser_convert.add_argument("-s", "--switch",
                     action="store_true",
                     help="id <--> vs_currency (monero to usd --> usd to monero)")
-    
     parser_convert.set_defaults(func=display_result)
-#    args = parser.parse_args()
-#    args.func(args)
-    
+    # List - arguments
     parser_list.add_argument("-a","--id_all",
                     help="displays all convertable coin ids (over 13000 items!)", action="store_true")
     parser_list.add_argument("-l","--id_less",
                     help="displays a shortened list of convertable coin ids", action="store_true")
     parser_list.add_argument("-v","--vs_list",
                     help="displays all vs currencies (~60items)", action="store_true")
-                    
     parser_list.set_defaults(func=display_list)
+    
     args = parser.parse_args()
-    
-    
     
     try:
         args.func(args)
     except AttributeError as e:
         msg = f"{e}.\n{Style.BRIGHT}Please run: {Fore.YELLOW}./cgk.py --help{Style.RESET_ALL}"
         panic(msg)
-            
-        
-        
-        
-        
-        
-#    try:
-#        if args.id and args.vs_currency:
-#            id = args.id
-#            vs_currency = args.vs_currency
-#            n = args.amount
-#            sw = args.switch
-#            x_price = calc_x_price(id,vs_currency,n,sw)
-#            display_result(id,vs_currency,n,x_price,sw)
-#    except AttributeError: 
-#        pass          
-
-#    try:
-#        if args.id_all:
-#            display_id_list()
-#        elif args.id_less:
-#            display_id_less()
-#        elif args.vs_list:
-#            display_vs_currencies()        
-#    except AttributeError:
-#        pass 
     
 
 if __name__ == '__main__':
